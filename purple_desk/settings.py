9d69f9146b34d7652ce95410ca97e7efb17c4e6c
QaziAbdulBaseer <baseer.abdul@sybrid.com>
2025-09-16 13:18:30 +0500

    Now your app is working on the mysql database

--- purple_desk/settings.py
+++ purple_desk/settings.py
@@ -100,12 +100,30 @@ WSGI_APPLICATION = 'purple_desk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'purple_desk_db',      # your database name
+        'USER': 'root',                # your MySQL username
+        'PASSWORD': '',   # your MySQL password
+        'HOST': '127.0.0.1',           # or 'localhost'
+        'PORT': '3306',                # default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
+
 
 
 # Password validation
